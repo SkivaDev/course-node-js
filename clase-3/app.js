4e30e0b5cca9541e1581e261faf1be0894a1d8be
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-11-30 21:16:34 -0500

    add class 3: restAPI get, post

---
+++ clase-3/app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const crypto = require("node:crypto");
+
+const movies = require("./movies.json");
+const { validateMovie } = require("./schemas/movies");
+
+const app = express();
+app.disable("x-powered-by"); // Deshabilita el header X-Powered-By: Express
+
+app.get("/", (req, res) => {
+  res.send("Hola Mundo!");
+});
+
+// Todos los recursos que sean MOVIES se identifican con /api/movies
+app.get("/api/movies", (req, res) => {
+  const { title, genre } = req.query;
+  let filteredMovies = [...movies];
+
+  // // Filtrar por titulo
+  if (title) {
+    filteredMovies = filteredMovies.filter((movie) =>
+      movie.title.toLowerCase().includes(title.toLowerCase())
+    );
+  }
+
+  // Filtrar por genero
+  if (genre) {
+    filteredMovies = filteredMovies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+  }
+
+  res.json(filteredMovies);
+});
+
+app.get("/api/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movie = movies.find((movie) => movie.id == id);
+  if (movie) {
+    res.json(movie);
+  } else {
+    res.status(404).send("Movie not found");
+  }
+});
+
+app.post("/api/movies", (req, res) => {
+  
+  const result = validateMovie(req.body)
+
+  if (!result.success) {
+    // 422 Unprocessable Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  // en base de datos
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data
+  }
+
+  // Esto no sería REST, porque estamos guardando
+  // el estado de la aplicación en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+});
+
+const PORT = process.env.PORT || 1234;
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
