83501f98a21465b18e4955040bf3cd767d9d2b1b
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-12-01 19:15:10 -0500

    feat: :sparkles: add CORS header permissions

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const crypto = require("node:crypto");
+const cors = require("cors");
 
 const movies = require("./movies.json");
-const { validateMovie } = require("./schemas/movies");
+const { validateMovie, validatePartialMovie } = require("./schemas/movies");
 
 const app = express();
+app.use(express.json()); // Middleware para parsear el body en JSON
+app.use(cors({
+  origin: (origin, callback) => {
+
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:3000',
+      'http://localhost:8080',
+      'https://fabrizioortiz.netlify.app'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+})); // Middleware para habilitar CORS
 app.disable("x-powered-by"); // Deshabilita el header X-Powered-By: Express
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Hola Mundo!");
@@ -13,6 +34,7 @@ app.get("/", (req, res) => {
 
 // Todos los recursos que sean MOVIES se identifican con /api/movies
 app.get("/api/movies", (req, res) => {
+
   const { title, genre } = req.query;
   let filteredMovies = [...movies];
 
@@ -45,7 +67,10 @@ app.get("/api/movies/:id", (req, res) => {
 
 app.post("/api/movies", (req, res) => {
   
-  const result = validateMovie(req.body)
+  const result = validateMovie({
+    "title" : "a",
+    "year": 2020
+  })
 
   if (!result.success) {
     // 422 Unprocessable Entity
@@ -63,6 +88,47 @@ app.post("/api/movies", (req, res) => {
   movies.push(newMovie)
 
   res.status(201).json(newMovie)
+});
+
+app.patch("/api/movies/:id", (req, res) => {
+  const result = validatePartialMovie(req.body)
+  
+  if (!result.success) {
+    // 422 Unprocessable Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id == id);
+
+  if (movieIndex === -1) {
+    return res.status(404).send("Movie not found");
+  }
+
+  const updatedMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updatedMovie
+
+  res.json(updatedMovie);
+
+})
+
+
+app.delete("/api/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id == id);
+
+  if (movieIndex === -1) {
+    return res.status(404).send("Movie not found");
+  }
+
+  movies.splice(movieIndex, 1);
+
+  res.status(204).json({ message: "Movie deleted" });
 });
 
 const PORT = process.env.PORT || 1234;
