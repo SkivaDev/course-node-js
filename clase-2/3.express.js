075590bf877ac6a64ee87cf1053bd9814d20ad8f
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-04-15 17:19:01 -0500

    feat(class-2): :sparkles: add 3.express.js and using middlewares

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -6,10 +6,29 @@ app.disable('x-powered-by')
 
 const PORT = process.env.PORT || 3000
 
-app.use((req, res, next) => {
-    console.log('El primer middleware')
-    next()
-})
+app.use(express.json())
+
+// Middleware para parsear el body de un request POST con Content-Type: application/json
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+
+//   // solo llegan request que son POST y que tienen el header Content-Type: application/json
+//   let body = ''
+
+//   // escuchar el evento data
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.timestamp = Date.now()
+//     // mutar la request y meter la información en el req.body
+//     req.body = data
+//     next()
+//   })
+// })
 
 app.get('/pokemon/ditto', (req, res) => {
     res.setHeader('Content-Type', 'application/json; charset=utf-8')
@@ -36,3 +55,7 @@ app.post('/pokemon', (req, res) => {
 app.use((req, res) => {
     res.status(404).send('<h1>404 Not Found</h1>')
 })
+
+app.listen(PORT, () => {
+    console.log(`Express server listening on port http://localhost:${PORT}`)
+  })
