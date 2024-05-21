770da385a2ac48945096fe12646a968a59c26aa6
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-05-21 18:51:31 -0500

    feat(class-6): :sparkles: creando el websocket

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import logger from 'morgan';
 
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+});
+
 app.use(logger('dev'));
 
 app.get('/', (req, res) => {
@@ -11,6 +21,6 @@ app.get('/', (req, res) => {
     }
 );
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
