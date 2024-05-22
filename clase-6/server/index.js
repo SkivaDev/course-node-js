277888b47f712646673bddfe2abdfd248fae9edd
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-05-22 18:21:20 -0500

    feat(class-6): :sparkles: Creando una bd

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
+import dotenv from 'dotenv';
+import { createClient } from '@libsql/client';
 
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
@@ -9,6 +11,12 @@ const port = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+
+const db = createClient({
+    url: 'libsql://kind-whizzer-skivadev.turso.io',
+    authToken: process.env.DB_TOKEN
+});
+
 
 io.on('connection', (socket) => {
     console.log('User connected');
