072d0801c09b9f54605abc465951ee8ad9e8aa6a
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-05-27 11:37:53 -0500

    feat(class-5): :sparkles: Filtrado por genero

--- clase-5/models/mysql/movie.js
+++ clase-5/models/mysql/movie.js
@@ -14,6 +14,28 @@ const connection = await mysql.createConnection(config);
 export class MovieModel {
     static async getAll({ genre }) {
 
+        if(genre) {
+            const lowerCaseGenre = genre.toLowerCase();
+            const [genres] = await connection.query('SELECT id FROM genre WHERE LOWER(name) = ?;', [lowerCaseGenre]);
+
+            if(genres.length === 0) {
+                return [];
+            }
+
+            const [{ id }] = genres;
+
+            //Query a movie_genres
+            const [moviesId] = await connection.query('SELECT movie_id FROM movie_genres WHERE genre_id = ?;', [id]);
+            // console.log(result);
+
+            //Join
+            const moviesIdArray = moviesId.map(({ movie_id }) => movie_id);
+            const [movies] = await connection.query('SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) FROM movie WHERE id IN (?);', [moviesIdArray]);
+
+            return movies;
+        }
+
+
         const [movies] = await connection.query('select title, year, director, duration, poster, rate, BIN_TO_UUID(id) from movie;');
 
         return movies;
