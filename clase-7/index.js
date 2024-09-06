3f218be8763d0d2ecd4d30d3036b06e29077bff9
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-09-06 17:02:45 -0500

    feat(class-7): :sparkles: Creando Endpoints GET & POST

--- clase-7/index.js
+++ clase-7/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { PORT } from "./config";
+import { PORT } from "./config.js";
 
 const app = express();
 
@@ -7,6 +7,29 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
     }
 );
+
+app.post('/login', (req, res) => {
+    res.send('Login successful!');
+    }
+);
+
+app.post('/register', (req, res) => {
+    res.send('Register successful!');
+    }
+);
+
+app.post('/logout', (req, res) => {
+    res.send('Logout successful!');
+    }
+);
+
+
+app.get('/protected', (req, res) => {
+    res.send('Protected route');
+    }
+);
+
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
