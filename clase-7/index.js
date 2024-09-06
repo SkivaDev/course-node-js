90f887452d1cf4f99589f9281181521935ccb5a2
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-09-06 16:26:38 -0500

    feat(class-7): :sparkles: Creando Servidor

---
+++ clase-7/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { PORT } from "./config";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+    }
+);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+    }
+);
